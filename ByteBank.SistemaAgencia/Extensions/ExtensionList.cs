6eb8e042163c84f008329196b1691c8ca721ed58
hunteradv <gustavopaulody@gmail.com>
2022-01-19 20:38:31 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Extensions/ExtensionList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia.Extensions
+{
+    public static class ExtensionList
+    {
+        public static void AddSeveral<T>(this List<T> IntList, params T[] items)
+        {
+            foreach (T item in items)
+            {
+                IntList.Add(item);
+            }
+        }
+    }
+}   
